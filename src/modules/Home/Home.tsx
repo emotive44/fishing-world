813c1dd5f6c0cc3744082f2d4008a1fe25303855
marko-streleshki <marko.streleshki@app-streams.com>
2021-09-16 19:59:46 +0300

    Setup css grid system

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+
+import { Input } from '../../shared/library';
+
 import classes from './Home.module.scss';
 
 const Home = () => {
   return (
-    <div className={classes.home}>Home</div>
+    <div className={classes.home}>
+      Home
+      <div className="row-4 row-m-wrap">
+        <div className="col-33 col-t-100 col-m-100 pad-4" style={{background: 'red'}}>
+        <Input
+            leftIcon
+            largeInput
+            labelInside
+            name              = "name"
+            type              = "text"
+            label             = "Username"
+            pattern           = {"[A-Z]+"}
+            value             = "fdsfsfsfddsf"
+            callbackChange    = {() => {}}
+            icon              = {<i className="fas fa-user" />}
+          />
+        </div>
+        <div className="col-33 col-t-50 col-m-100 pad-4" style={{background: 'green'}}>
+        <Input
+            leftIcon
+            largeInput
+            labelInside
+            name              = "name"
+            type              = "text"
+            label             = "Username"
+            pattern           = {"[A-Z]+"}
+            value             = "fdsfsfsfddsf"
+            callbackChange    = {() => {}}
+            icon              = {<i className="fas fa-user" />}
+          />
+        </div>
+        <div className="col-33 col-t-50 col-m-100 pad-4" style={{background: 'blue'}}>
+        <Input
+            leftIcon
+            largeInput
+            labelInside
+            name              = "name"
+            type              = "text"
+            label             = "Username"
+            pattern           = {"[A-Z]+"}
+            value             = "fdsfsfsfddsf"
+            callbackChange    = {() => {}}
+            icon              = {<i className="fas fa-user" />}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
